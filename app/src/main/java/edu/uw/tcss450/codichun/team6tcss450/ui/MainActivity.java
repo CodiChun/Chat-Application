330eb35c9550253bbb773bf0708ce95c4749b67e
Codi Chun <codichun@uw.edu>
2023-04-21 14:06:42 -0700

    created project and packages

---
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.uw.tcss450.codichun.team6tcss450.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.uw.tcss450.codichun.team6tcss450.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
