926ad3f624a12c8b314c167f987d87c1c43be870
Salahuddin Majed <salahuddinmajed@yahoo.com>
2023-05-01 10:49:38 -0700

    I had created the SignUp page. It asks for Username, Email, Password and Re-password. After clicking the register it will show you what's the username.

--- app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/MainActivity.java
@@ -3,6 +3,11 @@ package edu.uw.tcss450.codichun.team6tcss450.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 import edu.uw.tcss450.codichun.team6tcss450.R;
 
@@ -12,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = (EditText)  findViewById(R.id.username);
+        MaterialButton regbtn = (MaterialButton) findViewById(R.id.signupbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username1 = username.getText().toString();
+                Toast.makeText(MainActivity.this,"Username is "+" " + username1, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
