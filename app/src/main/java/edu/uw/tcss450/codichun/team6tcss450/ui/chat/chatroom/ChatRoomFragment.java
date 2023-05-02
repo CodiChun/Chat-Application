d5c6bcc541cc9d617c6f66b7969616dad0ee50ba
Codi Chun <codichun@uw.edu>
2023-05-01 22:33:05 -0700

    Added the card views for messages

--- app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/chat/chatroom/ChatRoomFragment.java
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/chat/chatroom/ChatRoomFragment.java
@@ -1,13 +1,10 @@
-package edu.uw.tcss450.codichun.team6tcss450.ui.chat;
+package edu.uw.tcss450.codichun.team6tcss450.ui.chat.chatroom;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import edu.uw.tcss450.codichun.team6tcss450.R;
 
 /**
@@ -15,12 +12,12 @@ import edu.uw.tcss450.codichun.team6tcss450.R;
  * @author codichun
  * @version 1.0
  */
-public class NewChatFragment extends Fragment {
+public class ChatRoomFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_chat, container, false);
+        return inflater.inflate(R.layout.fragment_chat_room, container, false);
     }
 }
