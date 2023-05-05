73b370eccf7067b6a669e7645dc25e81f3f5cebd
Julia Kulev <juliaykul@gmail.com>
2023-05-04 20:12:46 -0700

    temp branch that shows nav bar but doesnt show pages which it navigates to

---
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/HomeFragment.java
@@ -0,0 +1,40 @@
+package edu.uw.tcss450.codichun.team6tcss450.ui;
+
+import static androidx.databinding.DataBindingUtil.setContentView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import edu.uw.tcss450.codichun.team6tcss450.R;
+import edu.uw.tcss450.codichun.team6tcss450.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FragmentHomeBinding binding = FragmentHomeBinding.bind(getView());
+    }
+
+}
--- app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/ui/MainActivity.java
@@ -2,11 +2,16 @@
 package edu.uw.tcss450.codichun.team6tcss450.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 import android.os.Bundle;
 
@@ -19,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void bottomNav() {
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_chat,
+                R.id.navigation_contacts,
+                R.id.navigation_weather).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
