7897ecf17339efae18a33b05b5c320a4fe127c4c
Codi Chun <codichun@uw.edu>
2023-05-11 18:50:16 -0700

    added pushy token to sign in

--- app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/codichun/team6tcss450/MainActivity.java
@@ -5,18 +5,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.annotation.SuppressLint;
+
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import edu.uw.tcss450.codichun.team6tcss450.databinding.ActivityMainBinding;
+import edu.uw.tcss450.codichun.team6tcss450.model.NewMessageCountViewModel;
 import edu.uw.tcss450.codichun.team6tcss450.model.UserInfoViewModel;
+import edu.uw.tcss450.codichun.team6tcss450.services.PushReceiver;
+import edu.uw.tcss450.codichun.team6tcss450.ui.chat.chatroom.ChatMessage;
+import edu.uw.tcss450.codichun.team6tcss450.ui.chat.chatroom.ChatViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private MainPushMessageReceiver mPushMessageReceiver;
+
+    private NewMessageCountViewModel mNewMessageModel;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -54,6 +70,30 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
         //setSupportActionBar(findViewById(R.id.toolbar));
 
+        //gain access to the message count ViewModel:
+        mNewMessageModel = new ViewModelProvider(this).get(NewMessageCountViewModel.class);
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.navigation_chatlist) {
+                //When the user navigates to the chats page, reset the new message count.
+                //This will need some extra logic for your project as it should have
+                //multiple chat rooms.
+                mNewMessageModel.reset();
+            }
+        });
+
+        mNewMessageModel.addMessageCountObserver(this, count -> {
+            BadgeDrawable badge = binding.navView.getOrCreateBadge(R.id.navigation_chatlist);
+            badge.setMaxCharacterCount(2);
+            if (count > 0) {
+                //new messages! update and show the notification badge.
+                badge.setNumber(count);
+                badge.setVisible(true);
+            } else {
+                //user did some action to clear the new messages, remove the badge
+                badge.clearNumber();
+                badge.setVisible(false);
+            }
+        });
 
     }
 
@@ -64,5 +104,56 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    /**
+     * A BroadcastReceiver that listens for messages sent from PushReceiver
+     */
+    private class MainPushMessageReceiver extends BroadcastReceiver {
 
+        private ChatViewModel mModel =
+                new ViewModelProvider(MainActivity.this)
+                        .get(ChatViewModel.class);
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            NavController nc =
+                    Navigation.findNavController(
+                            MainActivity.this, R.id.nav_host_fragment);
+            NavDestination nd = nc.getCurrentDestination();
+
+            if (intent.hasExtra("chatMessage")) {
+
+                ChatMessage cm = (ChatMessage) intent.getSerializableExtra("chatMessage");
+
+                //If the user is not on the chat screen, update the
+                // NewMessageCountView Model
+                if (nd.getId() != R.id.navigation_chatlist) {
+                    mNewMessageModel.increment();
+                }
+                //Inform the view model holding chatroom messages of the new
+                //message.
+                mModel.addMessage(intent.getIntExtra("chatid", -1), cm);
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mPushMessageReceiver == null) {
+            mPushMessageReceiver = new MainPushMessageReceiver();
+        }
+        IntentFilter iFilter = new IntentFilter(PushReceiver.RECEIVED_NEW_MESSAGE);
+        registerReceiver(mPushMessageReceiver, iFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mPushMessageReceiver != null){
+
+
+
+        }
+        unregisterReceiver(mPushMessageReceiver);
+    }
 }
